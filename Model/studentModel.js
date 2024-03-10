d8d77f2ada6aacc6a492fe8663dfa63e1cc42823
ASUS <abhiraskar@gmail.com>
2024-03-10 10:19:17 +0530

    Backend Logic

--- Model/studentModel.js
+++ Model/studentModel.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const studentSchema = mongoose.Schema({
     name: { type: String, required: true },
     email: { type: String, required: true },
-    password: { type: String, required: true },
+    password: { type: String },
     contact: { type: Number, required: true },
     course: { type: String, required: true }
 }, {
